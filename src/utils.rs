589fd2528648283fc7dd7cda936dcda178fe2dfd
Matthias <matthias-endler@gmx.net>
2023-05-03 19:04:50 +0200

    fix lint

--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,18 @@
-use axum::{
-    extract::TypedHeader,
-    headers::UserAgent
-};
+use axum::{extract::TypedHeader, headers::UserAgent};
+use once_cell::sync::Lazy;
 use reqwest::header::HeaderValue;
 
-
-const DEFAULT_USER_AGENT: HeaderValue = HeaderValue::from_static(
-    concat!("Readable/", env!("CARGO_PKG_VERSION"))
-);
-
+static DEFAULT_USER_AGENT: Lazy<HeaderValue> =
+    Lazy::new(|| HeaderValue::from_static(concat!("Readable/", env!("CARGO_PKG_VERSION"))));
 
 pub fn forwarded_agent(ua_header: &Option<TypedHeader<UserAgent>>) -> HeaderValue {
     match ua_header {
         Some(TypedHeader(ua)) => {
-            HeaderValue::from_str(ua.as_str()).unwrap_or(DEFAULT_USER_AGENT)
-        },
-        None => DEFAULT_USER_AGENT,
+            HeaderValue::from_str(ua.as_str()).unwrap_or_else(|_| DEFAULT_USER_AGENT.clone())
+        }
+        None => DEFAULT_USER_AGENT.clone(),
     }
 }
-
 
 /// get current date and time as UTC
 /// and format as: 1 December, 2017 12:00:00
